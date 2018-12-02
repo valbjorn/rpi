0aec96363174432b5fb3f4dd12701c4229e5fa77
valbjorn <github-nikolaj@soevnig.dk>
2018-12-02 22:34:32 +0000

    added runninglight

--- experiment.py
+++ experiment.py
@@ -2,6 +2,7 @@ import bluetooth
 import time
 from gpiozero import LEDBoard
 from gpiozero.tools import random_values
+from gpiozero.tools import scaled
 from signal import pause
 tree = LEDBoard(*range(2,28),pwm=True)
 
@@ -10,15 +11,23 @@ def reset():
     led.off()
     led.source= [0]
 
+times=0.2
+
 def flicker():
  for led in tree:
     led.source_delay = 0.1
-    led.source = random_values()
+    led.source = scaled(random_values(),0,times)
+
 
 def isNikolajIn():
   result = bluetooth.lookup_name('94:65:2D:62:76:0C', timeout=5)
   if (result != None):
     print "Nikolaj: in"
+    global times
+    if times<0.6:
+      times += 0.1
+    else: 
+      times = 0.2
     return True
   else:
     print "Nikolaj: out"
